e4cb30d3cc1e8d7fbe0dcdac62ceca9938ba4185
00King00 <mr.1bilous@gmail.com>
2018-09-29 20:23:35 +0300

    before pull

---
+++ dev/AppConfig.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+	.training
+		h1 Math training hhhhhhhhhh0000000
+</template>
+
+<script>
+export default {
+	
+data () {
+	return {
+
+	}
+},
+methods:{
+},
+computed:{
+
+}
+}
+</script>
